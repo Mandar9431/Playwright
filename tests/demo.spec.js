2383de33396e4e5b023d97eeebbcf9780580a604
Mandar9431 <mandarkulkarni9431@gmail.com>
2024-04-17 22:48:16 +0900

    new test case

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -16,3 +16,12 @@ test("To verify title of the page", async({page}) =>{
     const title = page.locator('//*[@class="navbar__title text--truncate"]');
     await expect(title).toHaveText('Playwright');
 });
+
+test("To verify hyperlink 'GET STARTED' is present on lading page", async ({page}) => {
+    const landingPage = "https://playwright.dev/";
+    await page.goto(landingPage);
+    const title = page.locator('//*[@class="navbar__title text--truncate"]');
+    await expect(title).toHaveText('Playwright');
+    const GETSTARTED = page.locator('//a[@class="getStarted_Sjon"]');
+    await expect(GETSTARTED).toHaveText('Get started');
+});
