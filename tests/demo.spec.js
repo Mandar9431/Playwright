c25fc12c275794e446db4c0a19d9b76b74e994ed
Mandar9431 <mandarkulkarni9431@gmail.com>
2024-04-17 22:32:05 +0900

    basic structure

---
+++ tests/demo.spec.js
@@ -0,0 +1,18 @@
+// require packages
+const {test, expect} = require('@playwright/test');
+const exp = require('constants');
+
+//write test case
+// test case title 
+//async arrow function with page instace
+test("First test case", async ({page}) =>{
+    // Test steps
+    await page.goto("https://playwright.dev/");
+});
+
+test("To verify title of the page", async({page}) =>{
+    const landingPage = "https://playwright.dev/";
+    await page.goto(landingPage);
+    const title = page.locator('//*[@class="navbar__title text--truncate"]');
+    await expect(title).toHaveText('Playwright');
+});
